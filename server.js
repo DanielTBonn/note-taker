6ac68b7fdd3120ba1e47e582d6826237bed3c9c4
Daniel Bonn <danieltbonn@gmail.com>
2023-08-21 21:13:54 -0500

    added boilerplate for our server including the GET requests to public/index.html and public/notes.html

---
+++ server.js
@@ -0,0 +1,31 @@
+// essential variables for running our server and keeping track of data in a data base
+const express = require('express');
+const path = require('path')
+const fs = require('fs');
+const db = require('./db/db.json')
+
+const PORT = 3001;
+
+// initialize our server with express
+const app = express();
+
+// server will be able to read json and urlencoded POST requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// uses static files from the public directory
+app.use(express.static('public'));
+
+// the / route routes to public/index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+// the /notes route routes to public/notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port http://localhost:${PORT}`);
+})
